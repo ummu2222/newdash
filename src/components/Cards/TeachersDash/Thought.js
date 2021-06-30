eed4d016d53be3f399556d8d191fe655adcf13b6
ummu2222 <umangsomtiya8083@gmail.com>
2021-06-30 09:44:06 +0530

    thought

--- src/components/Cards/TeachersDash/Thought.js
+++ src/components/Cards/TeachersDash/Thought.js
@@ -7,13 +7,13 @@ const Thought = () =>{
         <h6 className="c-heading" >Thought of the Day</h6>
             <div className="row" >
                 <div style={{display:'flex',justifyContent:'space-around',alignItems:'center',fontFamily:'Montserrat'}}>
-                    <div id="quote" style={{fontSize:'34px'}}>
+                    <div id="quote" style={{fontSize:'28px'}}>
                         <span style={{color:'#e2b51c'}}>❝ </span>
                          <b>Live</b> as if you were to die tomorrow.
                         <b>Learn</b> as if you were to live forever 
                         <span style={{color:'#e2b51c'}}> ❞</span>
                     </div>
-                    <img src="../../../assets/Gandhi.png" style={{width:'350px',height:'350px'}}/>
+                    <img src="../../../assets/Gandhi.png" style={{width:'300px',height:'300px'}}/>
                 </div>
             </div>
         </div>
